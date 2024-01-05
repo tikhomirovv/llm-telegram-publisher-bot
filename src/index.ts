297b6113c9caf62a2e2c56fc8ee72d820916418b
Valerii Tikhomirov <v.tikhomirov.dev@gmail.com>
2024-01-05 22:33:27 +0300

    Rename `channel` -> `post`

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { sendPost } from "@/telegram-bot"
 
 const user: string = config.get("id")
 
-type Channel = {
+type Post = {
   chat_id: string
   schedule: string[]
   delay_window_min?: number
@@ -17,24 +17,24 @@ type Channel = {
   temperature?: number
 }
 
-const channels: Channel[] = config.get("channels")
-channels.forEach((channel) => {
-  scheduleStart(channel.schedule, async () => {
+const posts: Post[] = config.get("posts")
+posts.forEach((post) => {
+  scheduleStart(post.schedule, async () => {
     // time window
-    const delay = getDelay(channel.delay_window_min)
+    const delay = getDelay(post.delay_window_min)
     await sleep(delay)
 
     // generate prompt
-    let prompt: string = channel.prompt
-    if (channel.html) {
+    let prompt: string = post.prompt
+    if (post.html) {
       prompt += "\n\n" + getPromptHTML()
     }
-    const message = await generatePost(user, prompt, channel.temperature)
+    const message = await generatePost(user, prompt, post.temperature)
     logger.debug("[Index] Prompt:", prompt)
     logger.debug("[Index] Generated post:", message)
 
     // send
-    sendPost(channel.chat_id, message, channel.html ? "HTML" : "Markdown")
+    sendPost(post.chat_id, message, post.html ? "HTML" : "Markdown")
   })
 })
 
